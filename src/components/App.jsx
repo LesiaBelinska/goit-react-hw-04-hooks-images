ec7699ce80d7a1b51ee8addf63faa1e844947da2
LesiaBelinska <lm.kudak@gmail.com>
2022-05-05 17:31:13 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 
 import { Searchbar } from "./Searchbar/Searchbar.jsx";
 import { ImageGallery } from "./ImageGallery/ImageGallery.jsx";
@@ -9,58 +9,52 @@ import { pixabayFetchImage } from "services/api.js";
 
 import s from "./App.module.css";
 
-export class App extends Component {
+export const App = () => {
+  
+  const [search, setSearch] = useState('');
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(false);
 
-  state = {
-    search: '',
-    images: [],
-    error: null,
-    currentPage: 1,
-    isLoading: false,
-    hasMore: false,
-  };
+  useEffect(() => {
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.search !== this.state.search) {
-      this.fetchImage();
+    if (!search) {
+      return;
     }
-  }
 
-  handleSearchFormSubmit = newSearch => {
-    this.setState({ search: newSearch, images: [], error: null, currentPage: 1});
-  }
-
-  fetchImage = () => {
-
-    const { search, currentPage } = this.state;
-
-    if (search.trim() === '') {
-            alert('Enter search query')
-            return;
-        };
-
-    this.setState({ isLoading: true });
-
+    console.log('fetch')
+    setIsLoading(true);
     pixabayFetchImage(search, currentPage)
-      .then(({ images, total }) => {
-        const newImages = [...this.state.images, ...images];
-        const hasMore = newImages.length < total;
-        this.setState(prevState => ({
-          images: newImages,
-          currentPage: prevState.currentPage + 1,
-          hasMore,
-        }));
-    })
-      .catch(error => this.setState({ error }))
+      .then((data) => {
+        const newImages = [...images, ...data.images];
+        const hasMore = newImages.length < data.total;
+        setImages(newImages);
+        setHasMore(hasMore)
+       
+      })
+      .catch(error => setError(error))
       .finally(() => {
-        this.onLoadMoreButtonClick();
-          this.setState({ isLoading: false });
-        })
+        setIsLoading(false);
+      })
     
+  
+  }, [search, currentPage]);
+
+
+  const handleSearchFormSubmit = newSearch => {
+    
+    setSearch(newSearch);
+    setImages([]);
+    setError(null);
+    setCurrentPage(1);
   };
 
-  onLoadMoreButtonClick = () => {
-     if (this.state.currentPage > 1) {
+
+  const onLoadMoreButtonClick = () => {
+    setCurrentPage(prevState => prevState + 1);
+    if (currentPage > 1) {
       const options = {
         top: null,
         behavior: 'smooth',
@@ -71,24 +65,58 @@ export class App extends Component {
         window.scrollTo(options);
       }, 500);
     }
-  }
-
-
-  render() {
-
-    const { error, isLoading, images, hasMore } = this.state;
+  };
     
-    return (
-      <div className={s.App}>
-        <Searchbar onSubmit={this.handleSearchFormSubmit} />
-        {error && <p className={s.Error}>No matches found! Try again!</p>}
-        {isLoading &&
-          <Loader />}
-        <ImageGallery images={images} />
-        {hasMore &&
-          <Button onClick={this.fetchImage} />}
+  return (
+    <div className={s.App}>
+      <Searchbar onSubmit={handleSearchFormSubmit} />
+      {error && <p className={s.Error}>No matches found! Try again!</p>}
+      {isLoading &&
+        <Loader />}
+      <ImageGallery images={images} />
+      {hasMore &&
+        <Button onClick={onLoadMoreButtonClick} />}
+    </div>
+  )
+};
 
-      </div>
-    )
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fetchImage = () => {
+
+
+  //   if (search.trim() === '') {
+  //           alert('Enter search query')
+  //           return;
+  //       };
+
+  //   this.setState({ isLoading: true });
+
+  //   pixabayFetchImage(search, currentPage)
+  //     .then(({ images, total }) => {
+  //       const newImages = [...this.state.images, ...images];
+  //       const hasMore = newImages.length < total;
+  //       this.setState(prevState => ({
+  //         images: newImages,
+  //         currentPage: prevState.currentPage + 1,
+  //         hasMore,
+  //       }));
+  //   })
+  //     .catch(error => this.setState({ error }))
+  //     .finally(() => {
+  //       this.onLoadMoreButtonClick();
+  //         this.setState({ isLoading: false });
+  //       })
+    
+  // };
